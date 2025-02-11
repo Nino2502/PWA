d0058bdf1c572019496d20d5d1ae3576c8f122e7
Nino2502 <ninoleal2469@gmail.com>
2025-02-10 18:28:59 -0600

    Mi primer commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  email: string = '';
+  password: string = '';
+  isValid: boolean = false;
+  isModalOpen = false;
+  constructor() {}
+
+  alerta_nino(isOpen: boolean){
+
+    this.isModalOpen = isOpen;
+    console.log("Tuvo que llegar TRUE,", this.isModalOpen)
+
+    if(isOpen){
+      setTimeout(()=> {
+        this.isModalOpen = false;
+
+      }, 5000);
+    }
+  }
+  validarCampos() {
+    //SE VAN A CONVERIR TODAS LAS MAYUSCULAS EN MINUSCULAS EN 
+    ///AUTOMATICO
+    this.email = this.email.trim().toLowerCase();
+    ///vA A GUARDAR TODOS LOS INPUT SIN ESPACIOS
+    this.password = this.password.trim().replace(/\s/g, '');
+    //isValid si cumple esas condiciones se va a cambiar a true
+    this.isValid = this.email !== '' && this.password !== '';
+  }
+  entrar_inicio(){
+
+    this.validarCampos();
+    if(this.isValid){
+      ///Aqui mando un true a la funcion para mostrar la modal
+      this.alerta_nino(true);
+    }else{
+      alert("No cumple con las condiciones");
+      this.isValid = false;
+    }
+  }
+}
